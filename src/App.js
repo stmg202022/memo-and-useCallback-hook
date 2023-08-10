0010f022bf58f75975e109c6781a8cd3c24070c5
Samson Tmg <stmg202023@gmail.com>
2023-08-10 14:30:06 +0545

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UseMemo from "./Component/useMemo/useMemo";
 function App() {
   return (
     <div className="App">
+      <h1>LET'S SEE ON CONSOLE </h1>
       <Parent />
       <UseMemo />
     </div>
